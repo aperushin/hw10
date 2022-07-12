b2a81b7aef95d5ba9355d1eb4f42076621cd6feb
aperushin <aperushin@gmail.com>
2022-07-12 20:31:40 +0400

    remove unused method get_info()

--- candidate.py
+++ candidate.py
@@ -14,11 +14,3 @@ class Candidate:
 
     def __post_init__(self):
         self.skills_list = self.skills.lower().split(', ')
-
-    def get_info(self) -> dict:
-        candidate_info = dict(
-            name=self.name,
-            position=self.position,
-            skills=', '.join(self.skills_list)
-        )
-        return candidate_info
